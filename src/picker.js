ebcf724b64ca1832dc4156deb84b2866838db470
hareshRespo <106023720+hareshRespo@users.noreply.github.com>
2022-06-14 21:12:57 +0530

    Update picker.js

--- src/picker.js
+++ src/picker.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { ColorPropType, StyleSheet, View, ViewPropTypes as RNViewPropTypes, Text } from 'react-native';
 import PropTypes from 'prop-types';
 import WheelCurvedPicker from './WheelCurvedPicker';
+//test
 
 const ViewPropTypes = RNViewPropTypes || View.propTypes;
 
